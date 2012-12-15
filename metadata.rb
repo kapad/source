5a3bc5bc9380367870d58b3aa76b5e29dd2f4c37
Avishai Ish-Shalom <avishai@fewbytes.com>
2012-12-15 19:55:15 +0200

    updated metadata

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,9 @@ name             "source"
 maintainer       "Fewbytes"
 maintainer_email "chef@fewbytes.com"
 license          "All rights reserved"
-description      "Installs/Configures source"
+description      "Installs/builds source packages"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 
+suggests         "build-essential"
+suggests         "git"
