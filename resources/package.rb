6c8e6d8729912339ca7628f682dd1660411db4f0
Avishai Ish-Shalom <avishai@fewbytes.com>
2012-12-15 22:48:09 +0200

    fixed regex. sigh.

--- resources/package.rb
+++ resources/package.rb
@@ -1,7 +1,7 @@
 actions :build
 default_action :build
 
-attribute :source, :kind_of => [String], :regex => /(ftp|git|https?):\/\/[^\/]\/.*/
+attribute :source, :kind_of => [String], :regex => /(ftp|git|https?):\/\/[^\/]+\/.*/
 attribute :source_type, :kind_of => [String], :equal_to => ["git", "tarball"], :required => true
 attribute :creates, :kind_of => [String], :required => true
 attribute :checksum, :kind_of => [String], :regex => "[0-9a-fA-F]{64}", :default => nil
