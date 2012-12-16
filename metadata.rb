1f13c79d7783d1d0472f508d5cac5a6ca3a7a8f1
Avishai Ish-Shalom <avishai@fewbytes.com>
2012-12-16 11:35:33 +0200

    Add default recipe to install deps

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,5 @@ description      "Installs/builds source packages"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 
-suggests         "build-essential"
+depends         "build-essential"
 suggests         "git"
--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,7 @@
 # 
 # All rights reserved - Do Not Redistribute
 #
+include_recipe "build-essential"
+if node.platform_family == "debian"
+  package "pkg-config" # missing on build-essential
+end
