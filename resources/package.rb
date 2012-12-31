3afd220d4e842efbe6b7dc7cfdc3a221af89bc71
Yoni Yalovitsky <yoni@fewbytes.com>
2012-12-31 11:12:50 +0200

    minor refactoring

--- resources/package.rb
+++ resources/package.rb
@@ -5,7 +5,7 @@ attribute :source, :kind_of => [String], :regex => /(ftp|git|https?):\/\/[^\/]+\
 attribute :source_type, :kind_of => [String], :equal_to => ["git", "tarball"], :required => true
 attribute :creates, :kind_of => [String, Array], :required => true
 attribute :checksum, :kind_of => [String], :regex => "[0-9a-fA-F]{64}", :default => nil
-attribute :ref, :kind_of => String, :default => nil
+attribute :ref, :kind_of => String, :default => nil #revision
 attribute :build_command, :default => "make install", :kind_of => String
 attribute :environment, :default => {}, :kind_of => Hash
 
